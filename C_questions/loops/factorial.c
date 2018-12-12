d8b82932d1640f25a73e9524f11c7f45efd10d96
Mehmet Egemen <mehmetegemenalbayrak@gmail.com>
2018-12-12 11:39:24 +0300

    extended

--- C_questions/loops/factorial.c
+++ C_questions/loops/factorial.c
@@ -13,3 +13,45 @@ main()
     }
     printf("%d", c);
 }
+
+// Way 2, Taken from Let Us C Page 190
+
+main() {
+    int a, fact;
+    printf("Enter any number \n");
+    scanf("%d", &a);
+
+    fact = factorial( a );
+    printf("factorial value is %d", fact);
+}
+
+factorial (int x) {
+    int f = 1, i;
+
+    for (i = x; i >= 1;i--)
+        f = f * i;
+
+    return(f);
+}
+
+// Way 3 Recursive version
+
+main () {
+    int a, fact;
+    printf("Enter any number \n");
+    scanf("%d", &a);
+
+    fact = rec( a );
+    printf("factorial value is %d", fact);
+}
+
+rec (int x) {
+    int f;
+
+    if (x == 1)
+        return(1);
+    else
+        f = x * rec ( x - 1 );
+    
+    return(f);
+}
